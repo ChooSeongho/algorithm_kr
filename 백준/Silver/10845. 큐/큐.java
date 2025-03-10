220a11ed4ab7f7f88c9f701fb0b8f66c16e230ed
ChooSeongho <csh9895@naver.com>
2025-03-10 23:09:48 +0900

    [Silver IV] Title: 큐, Time: 168 ms, Memory: 19996 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        Queue<Integer> q = new LinkedList<Integer>();
+        while (N-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String cmd = st.nextToken();
+            if(cmd.equals("push")) {
+                q.add(Integer.parseInt(st.nextToken()));
+            } else if(cmd.equals("pop")) {
+                bw.write(q.isEmpty()? "-1" : String.valueOf(q.poll()));
+                bw.newLine();
+            } else if(cmd.equals("size")) {
+                bw.write(String.valueOf(q.size()));
+                bw.newLine();
+            } else if(cmd.equals("empty")) {
+                bw.write(q.isEmpty()? "1" : "0");
+                bw.newLine();
+            } else if(cmd.equals("front")) {
+                bw.write(q.isEmpty()? "-1" : String.valueOf(q.peek()));
+                bw.newLine();
+            } else if(cmd.equals("back")) {
+                bw.write(q.isEmpty()? "-1" : String.valueOf(q.toArray()[q.size() - 1]));
+                bw.newLine();
+            }
+        }
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
